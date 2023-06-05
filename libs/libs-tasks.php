28dc46ae47fd7571108d0fbfad190c3e6ed423c6
Ehsan-Pazhman <ehsanpejman1@gmail.com>
2023-06-05 09:11:41 +0430

    delete task implemented

--- libs/libs-tasks.php
+++ libs/libs-tasks.php
@@ -32,3 +32,22 @@ function addTask($taskTitle,$folderId){
     return $stmt->rowCount();
 }
 /* پایان تابع */
+
+/* تابع حذف یک تسک از دیتابیس */
+function deleteTask($taskId){
+    global $conn;
+    $folderId = $_GET['folderId'] ?? null;
+    $folderCondition = '';
+    if (isset($folderId) and is_numeric($folderId)){
+        $folderCondition = " and folder_id=$folderId";
+    }
+    $sql = "DELETE FROM tasks WHERE id = $taskId $folderCondition";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount();
+}
+// شرط اعتبار سنجی آیدی تسکی که قراره حذف بشه که در صورت برقراری آن تسک مورد نظر حذف خواهد شد
+if (isset($_GET['deleteTask']) and is_numeric($_GET['deleteTask'])){
+    deleteTask($_GET['deleteTask']);
+}
+/* پایان تابع */
